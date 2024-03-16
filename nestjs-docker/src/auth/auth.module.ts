8a3cc4a8f6e003b6434ed51361189cb3fe55d384
AHMED BELLAAJ <ahmed.bellaaj@ipsas.tn>
2024-03-16 22:21:35 +0000

    auth.modules.ts commit

--- nestjs-docker/src/auth/auth.module.ts
+++ nestjs-docker/src/auth/auth.module.ts
@@ -5,12 +5,21 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
 import { UserSchema } from './schemas/user.schema';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: jwt}),
     JwtModule.registerAsync({
-
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          secret: config.get<string>('JWT_SECRET'),
+          signOptions: {
+            expiresIn: config.get<string | number>('JWT_EXPIRES'),
+          },
+        }
+      }
     }),
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema}])
   ],
